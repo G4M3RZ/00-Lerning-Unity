96e788d1b8a0da25ed1475bfc436ecd77fa983ba
Pablo <pablo.0698@hotmail.com>
2020-06-24 13:24:58 -0500

    Unity Created & gitignore added

---
+++ Assets/Scripts/Materials/AffectEmission.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AffectEmission : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float _maxIntensity, _minIntensity, _speed;
+    private Material _sphere;
+
+    private void Start()
+    {
+        _sphere = GetComponent<MeshRenderer>().material;
+    }
+    private void Update()
+    {
+        float emission = _minIntensity + Mathf.PingPong(Time.time * _speed, _maxIntensity - _minIntensity);
+        Color finalColor = Color.white * Mathf.LinearToGammaSpace(emission);
+        _sphere.SetColor("_EmissionColor", finalColor);
+    }
+}
